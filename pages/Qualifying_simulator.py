48414749e7f7bcb3f062ac03f6cb65113b11076f
Stuart Wilson <stuwilson98@gmail.com>
2026-05-09 21:23:28 +0100

    Enhance caching for load_session function
    Updated load_session function to include hash function for caching.

--- pages/Qualifying_simulator.py
+++ pages/Qualifying_simulator.py
@@ -29,7 +29,7 @@ gp = st.selectbox("Grand Prix", f1.get_event_schedule(year, include_testing=Fals
 
 
 # Load session and cache ---
-@st.cache_data(show_spinner="Downloading the data...")
+@st.cache_data(show_spinner="Downloading the data...", hash_funcs={f1.core.Session: lambda s: (s.event.EventName, s.event.year)})
 def load_session(year, gp):
     session = f1.get_session(year, gp, 'Q')
     session.load(laps=True, telemetry=False, weather=False, messages=False)
