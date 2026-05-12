60cd2aa887d5ea3f3ed53ab16ec9f43f19802913
Stuart Wilson <stuwilson98@gmail.com>
2026-05-12 11:25:46 +0100

    Refactor session loading and update cache usage

--- pages/Qualifying_simulator.py
+++ pages/Qualifying_simulator.py
@@ -29,7 +29,7 @@ gp = st.selectbox("Grand Prix", f1.get_event_schedule(year, include_testing=Fals
 
 
 # Load session and cache ---
-@st.cache_data(show_spinner="Downloading the data...")
+@st.cache_resource(show_spinner="Downloading the data...")
 def load_session(year, gp):
     session = f1.get_session(year, gp, 'Q')
     session.load(laps=True, telemetry=False, weather=False, messages=False)
